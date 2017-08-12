9805a11ca94509e192fffe6b849eef9cc2d56ab6
Md Rahaman <soikatesc@gmail.com>
2017-08-11 20:30:15 -0500

    completed CURD operation for MessageResources

--- src/main/java/org/soikat/esc/messenger/resources/MessageResource.java
+++ src/main/java/org/soikat/esc/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.soikat.esc.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,10 +33,30 @@ public class MessageResource {
 		return messageService.addMessage(message);
 	}
 	
+	@PUT
+	@Path("/{ messageId }")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{ messageId }")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+	
+	
 	@GET
 	@Path("/{ messageId }")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Message test(@PathParam("messageId") long messageId) {
+	public Message getMessage(@PathParam("messageId") long messageId) {
 		return messageService.getMessage(messageId);
 	}
 }
+
+
+
