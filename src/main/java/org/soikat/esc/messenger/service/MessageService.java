fd9303993d23b4bf318b636de662fe6079e2979b
Md Rahaman <soikatesc@gmail.com>
2017-08-11 19:33:19 -0500

    added fake database class and created hasmap for temporary storage

--- src/main/java/org/soikat/esc/messenger/service/MessageService.java
+++ src/main/java/org/soikat/esc/messenger/service/MessageService.java
@@ -2,19 +2,43 @@ package org.soikat.esc.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.soikat.esc.messenger.database.DatabaseClass;
 import org.soikat.esc.messenger.model.Message;
 
 public class MessageService {
 	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1, "Hello World", "Md"));
+		messages.put(2L, new Message(2, "Hello Jersy", "Md"));
+	}
+	
 	public List<Message> getAllMessages() {
-		Message m1 = new Message(1L, "Hello World", "Md Rahaman");
-		Message m2 = new Message(2L, "Hello Jersey!", "Md Rahaman");
-		
-		List<Message> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		
-		return list;
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0 ) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
 	}
 }
